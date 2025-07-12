9961a3c8b2c860c92f442d77052996a1c04e38fc
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-07-12 18:26:29 +0530

    setup react-router-dom

--- Frontend/React/src/App.jsx
+++ Frontend/React/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter([
+    {
+      path : "",
+      element : <></>
+    },
+    {
+      path : "",
+      element : <></>
+    },
+  ])
+
   return (
     <>
-    
+        <RouterProvider router={router} />
     </>
   )
 }
